f52023f35232d60d36d131d45a376e829d6a6643
Herb Stahl <ghstahl@gmail.com>
2020-03-05 16:10:35 -0800

    starting eventhub stuff

---
+++ src/EventHubsSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubsSender
+{
+
+    class Program
+    {
+        static string GuidS => Guid.NewGuid().ToString();
+        static string ConnectionString { get; set; }
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("https://docs.microsoft.com/en-us/azure/event-hubs/get-started-dotnet-standard-send-v2");
+            Console.Write("Enter Connection String: ");
+
+            string myFirstName;
+            ConnectionString = Console.ReadLine();
+
+            var helloWorld = await GetHelloWorldAsync();
+            Console.WriteLine(helloWorld);
+        }
+        //Endpoint=sb://<namespace>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<secret>
+        static async Task<string> GetHelloWorldAsync()
+        {
+            var message = $"test-{GuidS}";
+            //  await using (EventHubProducerClient producerClient = new EventHubProducerClient("evhns-shorturl-001", "evh-shorturl", new DefaultAzureCredential()))
+
+            await using (EventHubProducerClient producerClient = new EventHubProducerClient(ConnectionString, "evh-shorturl"))
+            {
+                // create a batch
+                using (EventDataBatch eventBatch = await producerClient.CreateBatchAsync())
+                {
+
+                    // add events to the batch. only one in this case. 
+                    eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(message)));
+
+                    // send the batch to the event hub
+                    await producerClient.SendAsync(eventBatch);
+                }
+            }
+            return $"{DateTime.Now} - SENT{Environment.NewLine}{message}";
+        }
+    }
+}
