fef0a2a4fbae17dd059fce82cdf202e2724356d6
Herb Stahl <ghstahl@gmail.com>
2020-03-08 12:20:53 -0700

    good old storage account queues

---
+++ src/StorageQueue_Reader/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+
+namespace StorageQueue_Reader
+{
+    class Program
+    {
+        public static string ConnectionString { get; private set; }
+        public static CloudStorageAccount CloudStorageAccount { get; private set; }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues");
+           
+            if (args.Any())
+            {
+                ConnectionString = args[0];
+            }
+            else
+            {
+                Console.Write("Enter Connection String: ");
+                ConnectionString = Console.ReadLine();
+            }
+            Console.WriteLine($">> {ConnectionString} <<");
+            CloudStorageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            var helloWorld = await GetHelloWorldAsync();
+            Console.WriteLine(helloWorld);
+        }
+        static async Task<string> GetHelloWorldAsync()
+        {
+            TimeSpan sleepDelay = TimeSpan.FromSeconds(10);
+
+            for (var i = 0; i < 10; i++)
+            {
+                // Note: We used to add this to a list, but that is not necessary.
+                // Note: We assign to a variable just to supress a compiler warning
+                // that we aren't using await here. 
+
+ 
+                var task = Task.Run(() => WorkTask( sleepDelay));
+ 
+            }
+            Console.ReadLine();
+            return "d";
+        }
+        static async Task WorkTask(TimeSpan sleepDelay)
+        {
+            await Task.Yield();
+            while (true)
+            {
+                var queueClient = CloudStorageAccount.CreateCloudQueueClient();
+                var queue = queueClient.GetQueueReference("backend-processing");
+                queue.CreateIfNotExists();
+                var retrievedMessage = await queue.GetMessageAsync();
+                if(retrievedMessage != null)
+                {
+                    Console.WriteLine("Retrieved message with content '{0}'", retrievedMessage.AsString);
+
+                }
+
+
+                await Task.Delay(sleepDelay);
+                if(retrievedMessage != null)
+                {
+//                    await queue.DeleteMessageAsync(retrievedMessage);
+//                    Console.WriteLine("Deleted message");
+
+                }
+
+            }
+        }
+ 
+    }
+}
---
+++ src/StorageQueue_Sender/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageQueue_Sender
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public DateTime DirectiveDate { get; set; }
+    }
+    class Program
+    {
+        public static string GuidS => Guid.NewGuid().ToString();
+        public static string ConnectionString { get; private set; }
+        public static int Count { get; private set; }
+        public static CloudStorageAccount CloudStorageAccount { get; private set; }
+        public static int CountMax = 100;
+
+        public static int SafeGetInt(string value, int defaultValue)
+        {
+            if (!int.TryParse(value, out var parsed))
+                parsed = defaultValue;
+            return parsed;
+        }
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues");
+            Count = 1; 
+            if (args.Any())
+            {
+                ConnectionString = args[0];
+               
+                if(args.Count() > 1)
+                {
+                    Count = SafeGetInt(args[1], Count);
+                }
+            }
+            else
+            {
+                Console.Write("Enter Connection String: ");
+                ConnectionString = Console.ReadLine();
+            }
+            Console.WriteLine($">> {ConnectionString} <<");
+            CloudStorageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            var helloWorld = await GetHelloWorldAsync();
+            Console.WriteLine(helloWorld);
+        }
+        static async Task<string> GetHelloWorldAsync()
+        {
+            
+            var queueClient = CloudStorageAccount.CreateCloudQueueClient();
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("backend-processing");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+
+            if(Count >= CountMax)
+            {
+                Count = CountMax;
+            }
+            var i = 0;
+            for(; i < Count; i++)
+            {
+                var message = new Message
+                {
+                    Name = GuidS,
+                    DirectiveDate = DateTime.UtcNow
+                };
+                var json = JsonConvert.SerializeObject(message);
+
+                CloudQueueMessage cqMessage = new CloudQueueMessage(json);
+
+                await queue.AddMessageAsync(cqMessage);
+            }
+            return $"{i} messages added.";
+        }
+    }
+}
